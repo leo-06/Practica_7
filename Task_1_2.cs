37ee27bbc0892d734c6bc5f1a1f84fd0e2b72e2a
Алексей <99542489+leo-06@users.noreply.github.com>
2023-04-07 16:30:34 +0300

    Add files via upload

---
+++ Task_1_2.cs
@@ -0,0 +1,90 @@
+﻿string path = "C:\\recipes.txt";
+
+Dictionary<string, List<List<string>>> cook_book = addFileToCookBook(path);
+
+printDictionary(get_shop_list_by_dishes(cook_book, new List<string> { "Омлет", "Утка по-пекински", "Запеченный картофель" }, 1));
+
+Dictionary<string, List<List<string>>> addFileToCookBook(string path)
+{
+    string[] linesOfText = File.ReadAllLines(path);
+    Dictionary<string, List<List<string>>> cook_book = new Dictionary<string, List<List<string>>>();
+    int a = 0, b = 0;
+    bool endOfText = false;
+    while (!endOfText)
+    {
+        string dishName = linesOfText[0 + a];
+        List<List<string>> composition = new List<List<string>>();
+        for (int i = 0; i < Convert.ToInt32(linesOfText[1 + a]); i++)
+        {
+            List<string> ingridient = new List<string>();
+
+            ingridient.Add("ingredient_name");
+            int iOFS = linesOfText[2 + a + i].IndexOf('|');                 
+            ingridient.Add(linesOfText[2 + a + i].Substring(0, iOFS - 1));
+
+            ingridient.Add("quantity");
+            int iOSS = linesOfText[2 + a + i].IndexOf('|', iOFS + 1);      
+            ingridient.Add(linesOfText[2 + a + i].Substring(iOFS + 2, iOSS - iOFS - 3));
+
+            ingridient.Add("measure");
+            ingridient.Add(linesOfText[2 + a + i].Substring(iOSS + 2, linesOfText[2 + a + i].Length - iOSS - 2));
+
+            b++;
+            composition.Add(ingridient);
+        }
+        b += 2;
+
+        cook_book.Add(dishName, composition);
+        if (b == linesOfText.Length)
+            endOfText = true;
+        b += 1;
+        a = b;
+    }
+    return cook_book;
+
+}
+
+
+
+
+Dictionary<string, List<string>> get_shop_list_by_dishes(Dictionary<string, List<List<string>>> cookBook, List<string> dishes, int person_count)
+{
+    Dictionary<string, List<string>> shopList = new Dictionary<string, List<string>>();
+    foreach (string dish in dishes)
+    {
+        foreach (List<string> ingridients in cook_book[dish])
+        {
+            if (shopList.ContainsKey(ingridients.ElementAt(1)))
+            {
+                int amountOfIngridient = Convert.ToInt32(shopList[ingridients.ElementAt(1)].ElementAt(3));
+                amountOfIngridient += Convert.ToInt32(ingridients.ElementAt(3)) * person_count;
+                shopList[ingridients.ElementAt(1)].RemoveAt(3);
+                shopList[ingridients.ElementAt(1)].Add(amountOfIngridient.ToString());
+            }
+            else
+            {
+                List<string> lst = ingridients.GetRange(4, 2);
+                lst.Add("quantity");
+                lst.Add((Convert.ToInt32(ingridients.ElementAt(3)) * person_count).ToString());
+                shopList.Add(ingridients.ElementAt(1), lst);
+            }
+        }
+    }
+    return shopList;
+}
+
+
+
+void printDictionary(Dictionary<string, List<string>> ShopList)
+{
+    foreach (string ingridient in ShopList.Keys)
+    {
+        Console.Write(ingridient);
+        foreach (string prop in ShopList[ingridient])
+        {
+            Console.Write($" {prop}");
+        }
+        Console.WriteLine("");
+    }
+
+}
---
+++ Task_3.cs
@@ -0,0 +1,26 @@
+﻿List<string> File_text = new List<string>();
+File_text.Add("C:\\1.txt");
+File_text.Add("C:\\2.txt");
+File_text.Add("C:\\3.txt");
+Merge_files(File_text);
+void Merge_files(List<string> list)
+{
+    SortedDictionary<int, KeyValuePair<string, string[]>> file = new SortedDictionary<int, KeyValuePair<string, string[]>>();
+    string Text = string.Empty;
+    foreach (string p in list)
+    {
+        file.Add(File.ReadAllLines(p).Length, new KeyValuePair<string, string[]>(p, File.ReadAllLines(p)));
+    }
+    foreach (int amountOfLines in file.Keys)
+    {
+        Text += file[amountOfLines].Key;
+        Text += '\n';
+        Text += amountOfLines;
+        Text += '\n';
+        Text += String.Join('\n', file[amountOfLines].Value);
+        Text += "\n\n";
+    }
+    if (!File.Exists("C:\\a.txt"))
+        File.Create("C:\\a.txt");
+    File.WriteAllText("C:\\a.txt", Text);
+}
